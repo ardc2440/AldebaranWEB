7ba77ecb8ea75967ed6b0e3ac450bb9b9ee7083b
Javier Linares <javierl@gmlsoftware.com>
2024-03-15 12:16:08 -0500

    (fix): Arreglo en visualizacion del reporte de inventarios

--- src/Aldebaran.Web/Pages/ReportPages/Inventory/ViewModel/InventoryViewModel.cs
+++ src/Aldebaran.Web/Pages/ReportPages/Inventory/ViewModel/InventoryViewModel.cs
@@ -2,36 +2,37 @@
 {
     public class InventoryViewModel
     {
-        public List<InventoryLine> Lines { get; set; }
-    }
-    public class InventoryLine
-    {
-        public string LineName { get; set; }
-        public List<InventoryItem> Items { get; set; }
-    }
-    public class InventoryItem
-    {
-        public string InternalReference { get; set; }
-        public string ItemName { get; set; }
-        public List<InventoryReference> InventoryReferences { get; set; }        
-    }
-    public class InventoryReference
-    {
-        public string ReferenceName { get; set; }
-        public int AvailableAmount { get; set; }
-        public int FreeZone { get; set; }
-        public List<InventoryPurchaseOrder> PurchaseOrders { get; set; }
-    }
-    public class InventoryPurchaseOrder
-    {
-        public DateTime Date { get; set; }
-        public string Warehouse { get; set; }
-        public int Total { get; set; }
-        public List<InventoryActivity> Activity { get; set; }
-    }
-    public class InventoryActivity
-    {
-        public DateTime Date { get; set; }
-        public string Description { get; set; }
+        public List<Line> Lines { get; set; }
+
+        public class Line
+        {
+            public string LineName { get; set; }
+            public List<Item> Items { get; set; }
+        }
+        public class Item
+        {
+            public string InternalReference { get; set; }
+            public string ItemName { get; set; }
+            public List<Reference> References { get; set; }
+        }
+        public class Reference
+        {
+            public string ReferenceName { get; set; }
+            public int AvailableAmount { get; set; }
+            public int FreeZone { get; set; }
+            public List<PurchaseOrder> PurchaseOrders { get; set; }
+        }
+        public class PurchaseOrder
+        {
+            public DateTime Date { get; set; }
+            public string Warehouse { get; set; }
+            public int Total { get; set; }
+            public List<Activity> Activities { get; set; }
+        }
+        public class Activity
+        {
+            public DateTime Date { get; set; }
+            public string Description { get; set; }
+        }
     }
 }
