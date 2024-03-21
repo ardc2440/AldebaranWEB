eaae835ab908d7f03a6d46157e267080c924622f
ardc2440@gmail.com <ardc2440@gmail.com>
2024-03-20 21:54:41 -0500

    Completar Cargue de Data para el reporte de Ajustes de Inventario

--- src/Aldebaran.Web/Pages/ReportPages/Inventory Adjustments/InventoryAdjustmentsReport.razor.cs
+++ src/Aldebaran.Web/Pages/ReportPages/Inventory Adjustments/InventoryAdjustmentsReport.razor.cs
@@ -3,8 +3,10 @@ using Aldebaran.Application.Services.Models;
 using Aldebaran.Web.Pages.ReportPages.Inventory_Adjustments.Components;
 using Aldebaran.Web.Pages.ReportPages.Inventory_Adjustments.ViewModel;
 using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.JSInterop;
 using Radzen;
+using System.Collections.Generic;
 
 namespace Aldebaran.Web.Pages.ReportPages.Inventory_Adjustments
 {
@@ -25,17 +27,31 @@ namespace Aldebaran.Web.Pages.ReportPages.Inventory_Adjustments
 
         [Inject]
         protected IAdjustmentService AdjustmentService { get; set; }
+
+        [Inject]
+        protected IWarehouseService WarehouseService { get; set; }
+
+        [Inject]
+        protected IAdjustmentDetailService AdjustmentDetailService { get; set; }
+
+        [Inject]
+        protected IItemReferenceService ItemReferenceService { get; set; }
+
         #endregion
 
         #region Variables
         protected InventoryAdjustmentsFilter Filter;
         protected InventoryAdjustmentsViewModel ViewModel;
+        protected IEnumerable<Warehouse> warehouses;
+        protected IEnumerable<ItemReference> itemReferences;
         private bool IsBusy = false;
         #endregion
 
         #region Overrides
         protected override async Task OnInitializedAsync()
         {
+            warehouses = await WarehouseService.GetAsync();
+
             ViewModel = new InventoryAdjustmentsViewModel()
             {
                 Adjustments = (await GetAdjustmentsAsync()).ToList()
@@ -45,20 +61,107 @@ namespace Aldebaran.Web.Pages.ReportPages.Inventory_Adjustments
 
         #region Fill Data Report
 
-        async Task<IEnumerable<InventoryAdjustmentsViewModel.Adjustment>> GetAdjustmentsAsync(CancellationToken ct = default)
+        async Task<List<InventoryAdjustmentsViewModel.Adjustment>> GetAdjustmentsAsync(CancellationToken ct = default)
         {
-            return from adjustment in await AdjustmentService.GetAsync(ct)
-                   select new InventoryAdjustmentsViewModel.Adjustment
-                   {
-                       AdjustmentDate = adjustment.AdjustmentDate,
-                       AdjustmentId = adjustment.AdjustmentId,
-                       AdjustmentReason = adjustment.AdjustmentReason.AdjustmentReasonName,
-                       AdjustmentType = adjustment.AdjustmentType.AdjustmentTypeName,
-                       CreationDate = adjustment.CreationDate,
-                       Employee = adjustment.Employee.FullName,
-                       Notes = adjustment.Notes,
-                       Warehouses = new List<InventoryAdjustmentsViewModel.Warehouse>()
-                   };
+            var adjustments = new List<InventoryAdjustmentsViewModel.Adjustment>();
+
+            foreach (var adjustment in await AdjustmentService.GetAsync(ct))
+            {
+                adjustments.Add(new InventoryAdjustmentsViewModel.Adjustment
+                {
+                    AdjustmentDate = adjustment.AdjustmentDate,
+                    AdjustmentId = adjustment.AdjustmentId,
+                    AdjustmentReason = adjustment.AdjustmentReason.AdjustmentReasonName,
+                    AdjustmentType = adjustment.AdjustmentType.AdjustmentTypeName,
+                    CreationDate = adjustment.CreationDate,
+                    Employee = adjustment.Employee.FullName,
+                    Notes = adjustment.Notes,
+                    Warehouses = await GetAdjustmentsAsync(adjustment, ct)
+                });
+            }
+
+            return adjustments;
+        }
+
+        async Task<List<InventoryAdjustmentsViewModel.Warehouse>> GetAdjustmentsAsync(Adjustment adjustment, CancellationToken ct = default)
+        {
+            var warehouses = new List<InventoryAdjustmentsViewModel.Warehouse>();
+
+            foreach (var warehouseId in adjustment.AdjustmentDetails.DistinctBy(d => d.WarehouseId).Select(s => s.WarehouseId))
+            {
+                var warehouse = await WarehouseService.FindAsync(warehouseId, ct);
+
+                warehouses.Add(new InventoryAdjustmentsViewModel.Warehouse
+                {
+                    WarehouseId = warehouse.WarehouseId,
+                    WarehouseName = warehouse.WarehouseName,
+                    Lines = await GetWarehouseLines(adjustment, warehouse.WarehouseId, ct)
+                });
+            }
+
+            return warehouses;
+        }
+
+        async Task<List<InventoryAdjustmentsViewModel.Line>> GetWarehouseLines(Adjustment adjustment, int warehouseId, CancellationToken ct = default)
+        {
+            var reportLines = new List<InventoryAdjustmentsViewModel.Line>();
+
+            var adjustmentDetailbyWarehouse = adjustment.AdjustmentDetails.Where(w => w.WarehouseId == warehouseId);
+
+            foreach (var referenceId in adjustmentDetailbyWarehouse.DistinctBy(d => d.ReferenceId).Select(s => s.ReferenceId))
+            {
+                var referece = await ItemReferenceService.FindAsync(referenceId, ct);
+
+                reportLines.Add(new InventoryAdjustmentsViewModel.Line
+                {
+                    LineCode = referece.Item.Line.LineCode,
+                    LineName = referece.Item.Line.LineName,
+                    Items = await GetLineItems(adjustmentDetailbyWarehouse, ct)
+                }); ;
+            }
+
+            return reportLines.DistinctBy(d => d.LineCode).ToList();
+        }
+
+        async Task<List<InventoryAdjustmentsViewModel.Item>> GetLineItems(IEnumerable<AdjustmentDetail> adjustmentDetails, CancellationToken ct = default)
+        {
+            var reportItems = new List<InventoryAdjustmentsViewModel.Item>();
+
+            foreach (var referenceId in adjustmentDetails.DistinctBy(d => d.ReferenceId).Select(s => s.ReferenceId))
+            {
+                var referece = await ItemReferenceService.FindAsync(referenceId, ct);
+
+                reportItems.Add(new InventoryAdjustmentsViewModel.Item
+                {
+                    ItemName = referece.Item.ItemName,
+                    InternalReference = referece.Item.InternalReference,
+                    References = await GetItemReferences(adjustmentDetails, referece.ItemId, ct)
+                }); ; ;
+            }
+
+            return reportItems.DistinctBy(d => d.ItemName).ToList();
+        }
+
+        async Task<List<InventoryAdjustmentsViewModel.Reference>> GetItemReferences(IEnumerable<AdjustmentDetail> adjustmentDetails, int itemId, CancellationToken ct = default)
+        {
+            var reportReferences = new List<InventoryAdjustmentsViewModel.Reference>();
+
+            foreach (var adjustmentDetail in adjustmentDetails)
+            {
+                var referece = await ItemReferenceService.FindAsync(adjustmentDetail.ReferenceId, ct);
+
+                if (referece.ItemId == itemId)
+                {
+                    reportReferences.Add(new InventoryAdjustmentsViewModel.Reference
+                    {
+                        ReferenceName = referece.ReferenceName,
+                        ReferenceCode = referece.ReferenceCode,
+                        AvailableAmount = adjustmentDetail.Quantity
+                    });
+                }
+            }
+
+            return reportReferences;
         }
 
         #endregion
