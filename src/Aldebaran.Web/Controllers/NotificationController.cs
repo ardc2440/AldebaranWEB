a9e4f6c82b30fa7b1d18678201ffc5eed7542966
Javier Linares <javierl@gmlsoftware.com>
2024-05-15 11:41:19 -0500

    controller

---
+++ src/Aldebaran.Web/Controllers/NotificationController.cs
@@ -0,0 +1,31 @@
+﻿using Aldebaran.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aldebaran.Web.Controllers
+{
+    [Route("Notification/[action]")]
+    public class NotificationController : Controller
+    {
+        private readonly IPurchaseOrderNotificationService PurchaseOrderNotificationService;
+
+        [HttpPost]
+        public async Task<IActionResult> PurchaseOrderUpdate(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
+        {
+            var template = message.Body.Template;
+            var notificationId = message.Header.MessageUid;
+            await PurchaseOrderNotificationService.UpdateNotificationStatusAsync(notificationId, message.MessageDeliveryStatus.Success, message.MessageDeliveryStatus.Message, ct);
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CustomerOrderUpdate(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CustomerReservationUpdate(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
