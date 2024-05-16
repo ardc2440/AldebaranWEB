e324c55579c1b20b7a58d6c241d446ea72a452ca
ardc2440@gmail.com <ardc2440@gmail.com>
2024-05-15 22:14:11 -0500

    Agregar llamado a metodos Update para Customer Order y reservation desde el controller de Notifications

--- src/Aldebaran.Web/Controllers/NotificationController.cs
+++ src/Aldebaran.Web/Controllers/NotificationController.cs
@@ -7,9 +7,12 @@ namespace Aldebaran.Web.Controllers
     public class NotificationController : Controller
     {
         private readonly IPurchaseOrderNotificationService PurchaseOrderNotificationService;
+        private readonly ICustomerOrderNotificationService CustomerOrderNotificationService;
+        private readonly ICustomerReservationNotificationService CustomerReservationNotificationService;
+
 
         [HttpPost]
-        public async Task<IActionResult> PurchaseOrderUpdate(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
+        public async Task<IActionResult> PurchaseOrderUpdateAsync(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
         {
             var template = message.Body.Template;
             var notificationId = message.Header.MessageUid;
@@ -18,15 +21,23 @@ namespace Aldebaran.Web.Controllers
             return Ok();
         }
         [HttpPost]
-        public async Task<IActionResult> CustomerOrderUpdate(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
+        public async Task<IActionResult> CustomerOrderUpdateAsync(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            var template = message.Body.Template;
+            var notificationId = message.Header.MessageUid;
+            var status = message.MessageDeliveryStatus.Success ? Application.Services.Models.NotificationStatus.Success : Application.Services.Models.NotificationStatus.Error;
+            await CustomerOrderNotificationService.UpdateAsync(notificationId, status, message.MessageDeliveryStatus.Message, ct);
+            return Ok();
         }
 
         [HttpPost]
-        public async Task<IActionResult> CustomerReservationUpdate(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
+        public async Task<IActionResult> CustomerReservationUpdateAsync(Application.Services.Notificator.Model.MessageModel message, CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            var template = message.Body.Template;
+            var notificationId = message.Header.MessageUid;
+            var status = message.MessageDeliveryStatus.Success ? Application.Services.Models.NotificationStatus.Success : Application.Services.Models.NotificationStatus.Error;
+            await CustomerReservationNotificationService.UpdateAsync(notificationId, status, message.MessageDeliveryStatus.Message, ct);
+            return Ok();
         }
     }
 }
