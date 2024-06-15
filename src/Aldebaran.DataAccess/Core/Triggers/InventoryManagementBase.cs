f3360158cc90ef1a29d78219123ec6865703a17a
ardc2440@gmail.com <ardc2440@gmail.com>
2024-06-15 14:25:46 -0500

    actualizar el check de ISSoldOut segun la cantidad de InventoryQuantity de la referencia, para el envio al Automata de Existencias

--- src/Aldebaran.DataAccess/Core/Triggers/InventoryManagementBase.cs
+++ src/Aldebaran.DataAccess/Core/Triggers/InventoryManagementBase.cs
@@ -13,6 +13,7 @@
         {
             var inventoryEntity = await _context.ItemReferences.FindAsync(new object[] { referenceId }, cancellationToken) ?? throw new ArgumentNullException($"Referencia con id {referenceId} no encontrada");
             inventoryEntity.InventoryQuantity += (quantity * operatorInOut);
+            inventoryEntity.IsSoldOut = inventoryEntity.InventoryQuantity <= 0;
         }
 
         public async Task UpdateReservedQuantityAsync(int referenceId, int quantity, int operatorInOut, CancellationToken cancellationToken)
