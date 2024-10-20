da06ef316b22d947ec11ca4b1dc64153ee223303
ardc2440@gmail.com <ardc2440@gmail.com>
2024-10-19 19:49:45 -0500

    agregar trigger a la configuracion de ReferencesWarehouse

--- src/Aldebaran.DataAccess/Configuration/ReferencesWarehouseConfiguration.cs
+++ src/Aldebaran.DataAccess/Configuration/ReferencesWarehouseConfiguration.cs
@@ -15,6 +15,7 @@ namespace Aldebaran.DataAccess.Entities
             builder.HasOne(a => a.ItemReference).WithMany(b => b.ReferencesWarehouses).HasForeignKey(c => c.ReferenceId).OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_ITEM_ITEM_WAREHOUSE_ITEM_REFERENCE");
             builder.HasOne(a => a.Warehouse).WithMany(b => b.ReferencesWarehouses).HasForeignKey(c => c.WarehouseId).OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("FK_ITEM_WAREHOUSE_WAREHOUSE");
             builder.ToTable(tb => tb.HasTrigger("TRGINSERTREXISTENCIAS"));
+            builder.ToTable(tb => tb.HasTrigger("Trg_Minimum_Local_warehouse_Quantity_Alarm"));            
         }
     }
 }
